f76a3681c439856d1449f17056acff28c4d635d8
MatMath <mathieu.k.legault@gmail.com>
2016-11-17 11:44:20 +0100

    Adding Fix-Generic Axis for the Graph.

--- spaceReactRender/src/graph/genericDotGraph.jsx
+++ spaceReactRender/src/graph/genericDotGraph.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 
 import GraphBar from './graphBar.jsx'
+import styles from './lineGraph.scss'
+
 var imputArray = [
   [5,6,7,12],
   [5,2,10,2],
@@ -14,7 +16,11 @@ export default class GenericBarGraph extends React.Component {
     this.state = {
       arrayOfArray: imputArray,
       maximum: this.getInputmax(labelArray),
-      labelList: this.getInputLabels(labelArray)
+      labelList: this.getInputLabels(labelArray),
+      axis:{
+        x: 'Year',
+        y: 'Prices'
+      }
     }
   }
   getInputmax(labelArray) {
@@ -36,12 +42,37 @@ export default class GenericBarGraph extends React.Component {
   render() {
     const {arrayOfArray, maximum, labelList} = this.state;
     return(<div>
+      <svg version="1.2" className={`${styles.graph}`} aria-labelledby="title" role="img">
+        <title id="title">A line chart showing some information</title>
+        <g className={`${styles.grid} ${styles.x_grid}`} id="xGrid">
+          <line x1="90" x2="90" y1="5" y2="371"></line>
+        </g>
+        <g className={`${styles.grid} ${styles.y_grid}`} id="yGrid">
+          <line x1="90" x2="705" y1="370" y2="370"></line>
+        </g>
+          <g className={`${styles.labels} ${styles.x_labels}`}>
+          <text x="100" y="400">2008</text>
+          <text x="246" y="400">2009</text>
+          <text x="392" y="400">2010</text>
+          <text x="538" y="400">2011</text>
+          <text x="684" y="400">2012</text>
+          <text x="400" y="440" className={`${styles.label_title}`}>Year</text>
+        </g>
+        <g className={`${styles.labels} ${styles.y_labels}`}>
+          <text x="80" y="15">15</text>
+          <text x="80" y="131">10</text>
+          <text x="80" y="248">5</text>
+          <text x="80" y="373">0</text>
+          <text x="50" y="200" className={`${styles.label_title}`}>Price</text>
+        </g>
+      </svg>
       <h2>Success with {arrayOfArray.length}, {maximum}, {labelList[0]}</h2>
-      {arrayOfArray[0].map((dataSequence) => {
-        return (<div>
+      {/* {arrayOfArray[0].map((dataSequence, index) => {
+        return (<div key={index}>
           {dataSequence}
           </div>)
-      })}
+      })} */}
+
       </div>)
   }
 }
